07b8e15fcbd9cba8c08772feabfad125e791eb33
Nilay <91113915+BajiraoXD@users.noreply.github.com>
2023-12-12 17:19:03 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export const Label = () => {
+  return (
+    <div className="label">
+      <div className="talk-it-out">
+        Talk it <br />
+        &nbsp;&nbsp;out!!
+      </div>
+    </div>
+  );
+};
